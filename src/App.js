a36de5b92f93cff32c44299901b34116f1a2dfff
haixiangyan <haixiang6123@gmail.com>
2022-03-05 11:04:27 +0800

    feat: 添加长方形的展示

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ const App = () => {
 
   const audioRef = useRef(null);
   const canvasRef = useRef(null);
+  const barCanvasRef = useRef(null);
 
   const play = async () => {
     await audioRef.current.play();
     const stream = audioRef.current.captureStream();
     visualize(stream)
+    visualizeBar(stream);
   }
 
   const playUrl = async () => {
@@ -23,11 +25,11 @@ const App = () => {
 
   const record = async () => {
     // 生成流
-    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    const stream = await navigator.mediaDevices.getUserMedia({audio: true, video: false});
     visualize(stream)
   }
 
-  const visualize = (stream, shouldOutput = false) =>  {
+  const visualize = (stream, shouldOutput = false) => {
     // 创建解析器，以及音频上下文
     const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
     const analyser = audioCtx.createAnalyser()
@@ -92,6 +94,57 @@ const App = () => {
     draw()
   }
 
+  const visualizeBar = (stream, shouldOutput = false) => {
+    // 创建解析器
+    const audioCtx = new (window.AudioContext || window.webkitAudioContext)()
+    const analyser = audioCtx.createAnalyser();
+
+    // 获取音频源
+    const source = audioCtx.createMediaStreamSource(stream);
+    source.connect(analyser);
+
+    if (shouldOutput) {
+      analyser.connect(audioCtx.destination);
+    }
+
+    // 可视化
+    analyser.fftSize = 256;
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+
+    // 可视化
+    const WIDTH = barCanvasRef.current.width;
+    const HEIGHT = barCanvasRef.current.height;
+    const canvasCtx = barCanvasRef.current.getContext("2d");
+
+    // 清空
+    canvasCtx.clearRect(0, 0, WIDTH, HEIGHT);
+
+    const draw = () => {
+      // 递归调用
+      requestAnimationFrame(draw);
+
+      analyser.getByteFrequencyData(dataArray);
+
+      // 绘制图形
+      canvasCtx.fillStyle = 'rgb(200, 200, 200)';
+      canvasCtx.fillRect(0, 0, WIDTH, HEIGHT);
+
+      const barWidth = (WIDTH / bufferLength) * 2.5
+      let x = 0;
+
+      for (let i = 0; i < dataArray.length; i++) {
+        const barHeight = dataArray[i];
+
+        canvasCtx.fillStyle = 'rgb(' + (barHeight + 100) + ',50,50)';
+        canvasCtx.fillRect(x, HEIGHT - barHeight / 2, barWidth, barHeight / 2);
+
+        x += barWidth + 1;
+      }
+    }
+    draw();
+  }
+
   const onPreview = (e) => {
     const [file] = e.target.files;
 
@@ -102,8 +155,9 @@ const App = () => {
 
   return (
     <div className="App">
-      <audio ref={audioRef} src={audioUrl} controls onPlay={() => play()} />
+      <audio ref={audioRef} src={audioUrl} controls onPlay={() => play()}/>
       <canvas ref={canvasRef} width={500} height={300}/>
+      <canvas ref={barCanvasRef} width={500} height={300}/>
       <button onClick={play}>播放</button>
       <button onClick={playUrl}>播放URL</button>
       <button onClick={record}>录音</button>
